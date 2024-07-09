4c03ef3da6e6bbffef730f9f62451af3affb4cd0
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-07-09 20:34:40 -0300

    feat: java: add participant entity

---
+++ planner-java/src/main/java/br/com/rocketseat/hiokdev/planner_java/participant/Participant.java
@@ -0,0 +1,44 @@
+package br.com.rocketseat.hiokdev.planner_java.participant;
+
+import br.com.rocketseat.hiokdev.planner_java.trip.Trip;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "participants")
+public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+
+}
