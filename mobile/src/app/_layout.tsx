caae1c1ef3c157eb6c945aeaebbb00ab27578195
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-07-08 21:34:47 -0300

    feat: mobile: config status bar

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,11 +1,17 @@
 import { Slot } from 'expo-router';
 
 import '@/styles/global.css';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
 export default function Layout() {
   return (
     <View className="flex-1 bg-zinc-950">
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
       <Slot />
     </View>
   );
