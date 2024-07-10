318b5183d13428446d7c4f5175154ce83fd2d4f5
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-07-10 12:16:11 -0300

    feat: mobile: implements details tab in trip details screen

--- mobile/src/app/trip/details.tsx
+++ mobile/src/app/trip/details.tsx
@@ -1,11 +1,144 @@
-import { Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Alert, FlatList, Text, View } from 'react-native';
+import { Plus } from 'lucide-react-native';
+import { colors } from '@/styles/colors';
+import { Modal } from '@/components/modal';
+import { Input } from '@/components/input';
+import { Button } from '@/components/button';
+import { TripLink, TripLinkProps } from '@/components/tripLink';
+import { Participant, ParticipantProps } from '@/components/participant';
+import { validateInput } from '@/utils/validateInput';
+import { linkServer } from '@/server/link-server';
+import { participantServer } from '@/server/participant-server';
 
 export function Details({ tripId }: { tripId: string }) {
+  const [isCreatingLinkTrip, setIsCreatingLinkTrip] = useState(false);
+
+  const [links, setLinks] = useState<TripLinkProps[]>([]);
+  const [participants, setParticipants] = useState<ParticipantProps[]>([]);
+  const [linkTitle, setLinkTitle] = useState('');
+  const [linkURL, setLinkURL] = useState('');
+
+  const [showNewLinkModal, setShowNewLinkModal] = useState(false);
+
+  function resetNewLinkFields() {
+    setLinkTitle('');
+    setLinkURL('');
+    setShowNewLinkModal(false);
+  }
+
+  async function handleCreateTripLink() {
+    try {
+      if (!linkTitle.trim()) {
+        return Alert.alert('Link', 'Informe um título para o link.');
+      }
+      if (!validateInput.url(linkURL.trim())) {
+        return Alert.alert('Link', 'Link inválido!');
+      }
+      setIsCreatingLinkTrip(true);
+      await linkServer.create({
+        tripId,
+        title: linkTitle,
+        url: linkURL,
+      });
+      Alert.alert('Link', 'Link criado com sucesso!');
+      resetNewLinkFields();
+      await getTripLinks();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsCreatingLinkTrip(false);
+    }
+  }
+
+  async function getTripLinks() {
+    try {
+      const links = await linkServer.getLinksByTripId(tripId);
+      setLinks(links);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function getTripParticipants() {
+    try {
+      const participants = await participantServer.getByTripId(tripId);
+      setParticipants(participants);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getTripLinks();
+    getTripParticipants();
+  }, []);
+
   return (
     <View className="flex-1 mt-5">
-      <Text className="text-zinc-50 text-2xl font-semibold">
-        Details: {tripId}
+      <Text className="text-zinc-50 text-2xl font-semibold mb-2">
+        Links importantes
       </Text>
+
+      <View className="flex-1">
+        {links.length > 0 ? (
+          <FlatList
+            data={links}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => <TripLink data={item} />}
+            contentContainerClassName="gap-4"
+          />
+        ) : (
+          <Text className="text-zinc-400 font-regular text-base mt-2 mb-6">
+            Nenhum link adicionado.
+          </Text>
+        )}
+
+        <Button variant="secondary" onPress={() => setShowNewLinkModal(true)}>
+          <Plus color={colors.zinc[200]} size={20} />
+
+          <Button.Title>Cadastrar novo link</Button.Title>
+        </Button>
+      </View>
+
+      <View className="flex-1 border-t border-zinc-800 mt-6">
+        <Text className="text-zinc-50 text-2xl font-semibold my-5">
+          Convidados
+        </Text>
+
+        <FlatList
+          data={participants}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Participant data={item} />}
+          contentContainerClassName="gap-4 pb-44"
+        />
+      </View>
+
+      <Modal
+        title="Cadastrar link"
+        subtitle="Todos os convidados podem visualizar os links importantes."
+        visible={showNewLinkModal}
+        onClose={() => setShowNewLinkModal(false)}
+      >
+        <View className="gap-2 mb-3">
+          <Input variant="secondary">
+            <Input.Field
+              placeholder="Título do link"
+              onChangeText={setLinkTitle}
+            />
+          </Input>
+
+          <Input variant="secondary">
+            <Input.Field placeholder="URL" onChangeText={setLinkURL} />
+          </Input>
+        </View>
+
+        <View className="mt-1">
+          <Button isLoading={isCreatingLinkTrip} onPress={handleCreateTripLink}>
+            <Button.Title>Salvar link</Button.Title>
+          </Button>
+        </View>
+      </Modal>
     </View>
   );
 }
