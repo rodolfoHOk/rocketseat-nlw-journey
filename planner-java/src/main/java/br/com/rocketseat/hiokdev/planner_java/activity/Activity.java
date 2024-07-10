75eb1b300ea96d46d76ba1682803722fd2f3ce31
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-07-10 15:49:20 -0300

    feat: java: add activity entity

---
+++ planner-java/src/main/java/br/com/rocketseat/hiokdev/planner_java/activity/Activity.java
@@ -0,0 +1,42 @@
+package br.com.rocketseat.hiokdev.planner_java.activity;
+
+import br.com.rocketseat.hiokdev.planner_java.trip.Trip;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "activities")
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(name = "occurs_at", nullable = false)
+    private LocalDateTime occursAt;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+
+}
